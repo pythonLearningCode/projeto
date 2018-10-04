afc63fdc8301c82ede652e977c7e58e16c7e02c8
william <williamguilhermesouza@gmail.com>
2018-10-04 00:17:04 -0300

    first cmt

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class Gerenciador(ScreenManager):
+	pass
+
+
+class Menu(Screen):
+	pass 
+
+
+class Sala(Screen):
+	pass
+
+
+class Aplicativo(App):
+	def build(self):
+		return Gerenciador()
+
+
+if __name__ == '__main__':
+	Aplicativo().run()
